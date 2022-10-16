e2299481596599fa65f56e5ce9392d874da879d6
Amir Hamza <hamza_amir@yahoo.com>
2022-10-17 00:26:56 +0600

    876. Middle of the Linked List

---
+++ D_04_01_876_Middle_of_the_Linked_List.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class D_04_01_876_Middle_of_the_Linked_List
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+/*
+ 876. Middle of the Linked List
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+
+Example 2:
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ */
