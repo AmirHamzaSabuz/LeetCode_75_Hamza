e7ed2b9df106e230ed31df75de2da04a7506a0f0
Amir Hamza <hamza_amir@yahoo.com>
2022-10-17 01:22:03 +0600

    589. N-ary Tree Preorder Traversal

--- D_05_01_121_ Best_Time_to_Buy_and_Sell_Stock.cs
+++ D_05_01_121_ Best_Time_to_Buy_and_Sell_Stock.cs
@@ -22,13 +22,13 @@ public class D_05_01_121_ Best_Time_to_Buy_and_Sell_Stock
 }
 
 /*
+ 121. Best Time to Buy and Sell Stock
  You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
- 
 
 Example 1:
 
